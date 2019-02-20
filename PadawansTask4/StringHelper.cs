2007ddc04f2710baabf7ed8e709c2800284782c7
HelenSolovey <47430048+HelenSolovey@users.noreply.github.com>
2019-02-20 10:00:48 +0300

    add the body of the program

--- PadawansTask4/StringHelper.cs
+++ PadawansTask4/StringHelper.cs
@@ -6,8 +6,19 @@ namespace PadawansTask4
     {
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if(str == null)
+                            throw new ArgumentNullException();
+            
+
+            int count = 0;
+            string letters = "aAiIoOeEuU";
+            for (int i = 0; i < str.Length; i++)
+                for (int j = 0; j < letters.Length; j++)
+                    if (str[i] == letters[j])
+                        count++;
+
+            return count;
+
         }
     }
 }
--- PadawansTask4/Tests/PublicTest.cs
+++ PadawansTask4/Tests/PublicTest.cs
@@ -6,8 +6,10 @@ namespace PadawansTask4.Tests
     public class PublicTest
     {
         [TestCase("abracadabra", ExpectedResult = 5)]
+        [TestCase("ab rac adabra", ExpectedResult = 5)]
         [TestCase("sprppr", ExpectedResult = 0)]
         [TestCase("aaa", ExpectedResult = 3)]
+        [TestCase("aaAa", ExpectedResult = 4)]
         public int GetVowelCountTests(string source) => StringHelper.GetVowelCount(source);
     }
 }
